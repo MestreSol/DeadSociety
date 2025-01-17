c584d9494a2ec99c61be50e88a506d451704e448
MestreSol <mb.muitobosta@gmail.com>
2025-01-17 12:39:35 -0300

    Refatoramento GameStatistics.cs e GameManager.cs

---
+++ DeadSocity/Assets/Scripts/Core/GameStatistics.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameStatistics : MonoBehaviour
+    {
+        public static GameStatistics Instance { get; private set; }
+
+        private int daysSurvived = 2;
+        
+        public event Action<int> OnDaysSurvivedChanged;
+
+        private void Awake()
+        {
+            if(Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        public void IncrementDaysSurvived()
+        {
+            daysSurvived++;
+            Debug.Log($"Dias Sobrevividos: {daysSurvived}");
+            OnDaysSurvivedChanged?.Invoke(daysSurvived);
+        }
+        
+        public int GetDaysSurvived() => daysSurvived;
+        
+        public void ResetStatistics()
+        {
+            daysSurvived = 0;
+            OnDaysSurvivedChanged?.Invoke(daysSurvived);
+            
+            Debug.Log("Estatísticas resetadas");
+        }
+    }
+}
